948b7c609c1c663f1c4ed2048c9c3aafac3d2717
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2026-04-27 13:24:09 -0300

    feat: add is_admin attribute in user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,5 @@
 from app.core.database import Base
-from sqlalchemy import Column, UUID, String, DateTime
+from sqlalchemy import Column, UUID, String, DateTime, Boolean
 from sqlalchemy.sql import func
 import uuid
 
@@ -14,5 +14,6 @@ class User(Base):
     cep = Column("cep", String, nullable=False)
     address = Column("address", String, nullable=False)
     complement = Column("complement", String, nullable=False)
+    is_admin = Column("is_admin", Boolean, default=False, nullable=False)
     created_at = Column("created_at", DateTime(timezone=True), nullable=False, server_default=func.now())
     updated_at = Column("updated_at", DateTime(timezone=True), nullable=False, server_default=func.now())
