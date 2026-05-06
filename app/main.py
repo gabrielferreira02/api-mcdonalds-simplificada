2fe7aec3297303b3d3d20384deb619b6e260cf4d
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2026-05-06 13:48:45 -0300

    chore: register auth routes

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ app = FastAPI()
 
 from app.api.routes.category_routes import category_router
 from app.api.routes.product_routes import product_router
+from app.api.routes.auth_routes import auth_router
 
+app.include_router(auth_router)
 app.include_router(category_router)
 app.include_router(product_router)
