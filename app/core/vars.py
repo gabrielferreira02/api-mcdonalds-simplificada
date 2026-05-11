9579be5bc347df8f66088c8c4494d51a9cce236e
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2026-05-11 14:23:03 -0300

    chore: add stripe variables

--- app/core/vars.py
+++ app/core/vars.py
@@ -8,3 +8,5 @@ SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 SUPABASE_BUCKET = os.getenv("SUPABASE_BUCKET")
 ALLOWED_TYPES = ["image/jpeg", "image/png", "image/webp"]
+STRIPE_KEY = os.getenv("STRIPE_KEY")
+STRIPE_ENDPOINT_SECRET = os.getenv("STRIPE_ENDPOINT_SECRET")
